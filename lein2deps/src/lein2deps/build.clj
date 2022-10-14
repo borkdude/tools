007b110feb553a5c07590a252b1a232388debb79
Michiel Borkent <michielborkent@gmail.com>
2022-10-14 12:57:47 +0200

    Migrate to lein2deps lib

--- lein2deps/src/lein2deps/build.clj
+++
@@ -1,8 +0,0 @@
-(ns lein2deps.build
-  (:require [clojure.tools.build.api :as b]))
-
-(def basis (b/create-basis {:project "deps.edn"}))
-
-(defn compile-java [_]
-  (let [opts (get-in basis [:aliases :build :lein2deps/compile-java])]
-    (b/javac (assoc opts :basis basis))))
