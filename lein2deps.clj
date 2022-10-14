6bf3950a42f6ee81e064b9b04e83f66ece3a9be9
Michiel Borkent <michielborkent@gmail.com>
2022-10-14 16:50:24 +0200

    Bump lein2deps tool

--- lein2deps.clj
+++ lein2deps.clj
@@ -1,6 +1,6 @@
 #!/usr/bin/env bb
 
 (require '[babashka.deps :as deps])
-(deps/add-deps '{:deps {io.github.borkdude/lein2deps {:git/sha "30c12aa7b7a0167a4580cc8695916ef4deed3322"}}})
+(deps/add-deps '{:deps {io.github.borkdude/lein2deps {:git/sha "e26edeb114c9d88a5c4d3abb683306588fcaad13"}}})
 (require '[lein2deps.api])
 (apply lein2deps.api/-main *command-line-args*)
