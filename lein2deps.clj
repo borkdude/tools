813e475054c06e1ddce351473b2e5fbc9af3db75
Michiel Borkent <michielborkent@gmail.com>
2022-10-14 13:37:15 +0200

    Upgrade SHA

--- lein2deps.clj
+++ lein2deps.clj
@@ -1,6 +1,6 @@
 #!/usr/bin/env bb
 
 (require '[babashka.deps :as deps])
-(deps/add-deps '{:deps {io.github.borkdude/lein2deps {:git/sha "43ddacba5b59505cc9b4cacacefb683439047192"}}})
+(deps/add-deps '{:deps {io.github.borkdude/lein2deps {:git/sha "30c12aa7b7a0167a4580cc8695916ef4deed3322"}}})
 (require '[lein2deps.api])
 (apply lein2deps.api/-main *command-line-args*)
