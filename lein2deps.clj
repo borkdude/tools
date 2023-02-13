53d6317eb8569ffdbbe2630bf4838af2e04d4eac
Michiel Borkent <michielborkent@gmail.com>
2023-02-13 20:18:41 +0100

    update lein2deps

--- lein2deps.clj
+++ lein2deps.clj
@@ -1,6 +1,6 @@
 #!/usr/bin/env bb
 
 (require '[babashka.deps :as deps])
-(deps/add-deps '{:deps {io.github.borkdude/lein2deps {:git/sha "60e33d969f624143f03ebd3456a703e68c8b9ab7"}}})
+(deps/add-deps '{:deps {io.github.borkdude/lein2deps {:git/sha "4f6d4a0140ddea10c5ac28cc24b49643660f42f6"}}})
 (require '[lein2deps.api])
 (apply lein2deps.api/-main *command-line-args*)
